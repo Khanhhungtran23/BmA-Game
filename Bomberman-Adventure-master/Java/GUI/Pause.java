f40600bd8a2e947a3ba850bb88d9b311609735d5
ITCSIU21217 <118715549+ITCSIU21217@users.noreply.github.com>
2023-05-09 15:20:46 +0700

    Add Pause.java

---
+++ Bomberman-Adventure-master/Java/GUI/Pause.java
@@ -0,0 +1,46 @@
+package GUI;
+
+import Controls.KeyHandler;
+import Variables.Constant;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Pause{
+    KeyHandler keyH =Window.getKeyH();
+    BufferedImage pause;
+    GameScene gameScene;
+    boolean flag;
+
+    public static Pause instance = null;
+    public static Pause getInstance(GameScene gameScene){
+        if(Pause.instance== null){
+            Pause.instance= new Pause(gameScene);
+        }
+        return Pause.instance;
+    }
+    Pause(GameScene gameScene) {
+        this.gameScene = gameScene;
+        try {
+            pause = ImageIO.read(new File("src/main/resources/Menu/Pause.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void pauseGame(){
+        if(keyH.pausePressed){
+            flag = true;}
+        if(!keyH.pausePressed && flag) {
+            gameScene.isPaused = !gameScene.isPaused;//toggle
+            flag = flase;
+        }
+    }
+    public void draw(Graphic g2){
+        g2.setColor(new Color(0,0,0,150));
+        g2.fillRect(0,0,Constant.WIDTH,Constant.HEIGHT);
+        g2.drawImage(pause,0,0,Constant.WIDTH,Constant.HEIGHT,null);
+
+        }
+    }
