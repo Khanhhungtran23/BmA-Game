1d3b6e5b420841e29f1922e1ed97d3b19728b488
ITCSIU21217 <118715549+ITCSIU21217@users.noreply.github.com>
2023-05-09 16:18:54 +0700

    Add Tile .java

---
+++ Bomberman-Adventure-master/Java/GUI/Tile.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import java.awt.image.BufferedImage;
+
+public class Tile{
+    public BufferedImage image;
+    public boolean collision = false;
+    public boolean death = false;
+
+}
