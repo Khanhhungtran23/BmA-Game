69bce54c7ce01e3a6d1826c55ae092992a67a740
KhanhHung <tvkhhung03@gmail.com>
2023-05-14 22:12:35 +0700

    add new file

---
+++ Bomberman-Adventure-master/Java/Controls/CheckAvailable.java
@@ -0,0 +1,32 @@
+package Controls;
+import Entity.Bomb;
+import GUI.GameScene;
+import GUI.Window;
+
+public class CheckAvailable {
+    static KeyHandler keyH = Window.getKeyH();
+    static boolean spacePressed;
+
+    public static boolean checkAvailable(int x, int y) {
+        x = ((x + 16) / 48) * 48;
+        y = ((y + 16) / 48) * 48;
+        for (Bomb bomb : GameScene.getBombList()) {
+            if (bomb.getX() == x && bomb.getY() == y) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean plantBomb(int x, int y){
+        if (GameScene.getBombCounter() < GameScene.getBombSize()) {
+            if (keyH.spacePressed) {
+                spacePressed = true;
+            }
+            if (!keyH.spacePressed && spacePressed) {
+                spacePressed = false;
+                return CheckAvailable.checkAvailable(x, y);
+            }
+        }
+        return false;
+    }
+}
