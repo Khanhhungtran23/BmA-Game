c4ee4e4ea8182b68602f8a2cc9092771669f2132
ITCSIU21217 <118715549+ITCSIU21217@users.noreply.github.com>
2023-05-16 18:56:32 +0700

    Add Window.java

---
+++ Bomberman-Adventure-master/Java/GUI/Window.java
@@ -0,0 +1,128 @@
+package GUI;
+
+import Controls.KeyHandler;
+import Controls.MouseHandler;
+import Variables.Constant;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame implements Runnable {
+    public static Window window = null;
+    public boolean isRunning;
+
+    public int currentState;
+    public Scene currentScene;
+
+    static KeyHandler keyH = new KeyHandler();
+    static MouseHandler mouseH = new MouseHandler();
+
+    public int currentMapID;
+
+    public Window(int width, int height, String TITLE) {
+        //Window handler
+        setSize(width, height);
+        setTitle(TITLE);
+        setIconImage(new ImageIcon("src\\main\\resources\\Logo-1.png").getImage());
+
+        setResizable(false);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        addKeyListener(keyH);
+        addMouseListener(mouseH);
+        addMouseMotionListener(mouseH);
+
+        changeState(0); // starts at State 0 (Menu)
+        isRunning = true;
+    }
+
+
+    public static Window getWindow() {
+        if (Window.window == null) {
+            Window.window = new Window(Constant.WIDTH, Constant.HEIGHT, Constant.TITLE);
+        }
+        return Window.window;
+    }
+
+    public void changeState(int newState) {
+        currentState = newState;
+        switch (currentState) {
+            case 0 -> currentScene = new MenuScene();
+            case 1 ->{
+                currentMapID = 1;
+                currentScene = new GameScene();
+            }
+            case 2 -> {
+                currentMapID = 2;
+                currentScene = new GameScene();
+            }
+            case 3 -> currentScene = new GameDone(); //Finish Game
+            case 4 -> currentScene = new Credit(); // Credit
+            default -> {
+                System.out.println("Error: Invalid state");
+                currentScene = null;
+            }
+        }
+    }
+    public int getCurrentMapID() {
+        return currentMapID;
+    }
+
+    public void close() {
+        isRunning = false;
+    }
+
+
+    public void update() {
+        Image dbImage = createImage(getWidth(), getHeight());
+        Graphics dbg = dbImage.getGraphics();
+        this.draw(dbg);
+        getGraphics().drawImage(dbImage, 0, 0, this);
+
+        currentScene.update();
+    }
+
+    public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        currentScene.draw(g2);
+    }
+
+    public void run() {
+        double drawInterval = 1000000000.0 / Constant.FPS, delta = 0;
+        long lastTime = System.nanoTime(), currentTime, timer = 0;
+        int Count = 0;
+
+        while (isRunning) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                delta--;
+                Count++;
+            }
+
+            if (timer >= 1000000000) {
+//                System.out.println("FPS: " + Count);
+                Count = 0;
+                timer = 0;
+            }
+        }
+        this.dispose();
+    }
+    public int getCurrentState() {
+        return currentState;
+    }
+    public static KeyHandler getKeyH() {
+        return keyH;
+    }
+    public static MouseHandler getMouseH() {
+        return mouseH;
+    }
+}
